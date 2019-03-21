d985842dd3b559d0202a456abd4a2d92c0008613
henriquessb <hssb8@hotmail.com>
2019-03-21 17:31:57 -0300

    Changed quaternion to euler angle conversion from engine function to own implementation
    Changing the angle conversion function makes the euler angles arround X and Y axis to not be inverted

--- Source/ema_vr_rehab/BlueprintQuaternionLibrary.cpp
+++ Source/ema_vr_rehab/BlueprintQuaternionLibrary.cpp
@@ -40,7 +40,27 @@ FQuat UBlueprintQuaternionLibrary::RelativeRotation(FQuat Quat1, FQuat Quat2)
 
 FVector UBlueprintQuaternionLibrary::Euler(FQuat Quat1)
 {
-	FVector EulerOut = Quat1.Euler();
+	/*(x, y, z, w) = (q[0], q[1], q[2], q[3])
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll = math.atan2(t0, t1)
+    t2 = +2.0 * (w * y - z * x)
+    t2 = +1.0 if t2 > +1.0 else t2
+    t2 = -1.0 if t2 < -1.0 else t2
+    pitch = math.asin(t2)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw = math.atan2(t3, t4)
+    return [roll, pitch, yaw]*/
+	float t[5];
+	t[0] = 2.0 * (Quat1.W * Quat1.X + Quat1.Y * Quat1.Z);
+	t[1] = 1.0 - 2.0 * (Quat1.X * Quat1.X + Quat1.Y * Quat1.Y);
+	t[2] = 2.0 * (Quat1.W * Quat1.Y - Quat1.Z * Quat1.X);
+	t[2] = fabsf(t[2]) > 1.0 ? powf(-1.0, signbit(t[2]))/*t[2]/fabsf(t[2])*/ : t[2];
+	t[3] = 2.0 * (Quat1.W * Quat1.Z + Quat1.X * Quat1.Y);
+	t[4] = 1.0 - 2.0 * (Quat1.Y * Quat1.Y + Quat1.Z * Quat1.Z);
+	FVector EulerOut = FVector(atan2f(t[0], t[1]), asinf(t[2]), atan2f(t[3], t[4])) * 180.0 / PI;
+	//FVector EulerOut = Quat1.Euler();
 	return EulerOut;
 }
 
